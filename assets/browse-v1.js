d3ce086e4fea971d920eee27cc049f86664da80b
James Daniel Marrs Ritchey <75754139+jamesdanielmarrsritchey@users.noreply.github.com>
2022-04-04 21:55:24 -0700

    Update browse-v1.js

--- assets/browse-v1.js
+++ assets/browse-v1.js
@@ -1,11 +1,21 @@
-/////////This file is a work in progress. It isn't fully written, and hasn't been integrated yet. It will be used ONLY by index.html to provide a browseable/searchable list of all posts, and pages. ///////////
-//Get page number from url parameters (script is hard coded to return 12 results per page)
-
-//Get filters (e.g., keywords, content types) from url parameters
-
+///////// WARNING: This file is incomplete!!! It has not yet been integrated into the site. ///////////
+//Get filters (e.g., keywords, content types, page_number, quantity) from url parameters
+var url = window.location.href;
+var filter_keywords = url.replace(/.*keywords=/, "");
+var filter_keywords = filter_keywords.replace(/&.*/, "");
+var filter_keywords = filter_keywords.replace(/=.*/, "");
+var filter_types = url.replace(/.*types=/, "");
+var filter_types = filter_types.replace(/&.*/, "");
+var filter_types = filter_types.replace(/=.*/, "");
+var filter_page_number = url.replace(/.*page=/, "");
+var filter_page_number = filter_page_number.replace(/&.*/, "");
+var filter_page_number = filter_page_number.replace(/=.*/, "");
+var filter_quantity = url.replace(/.*quantity=/, "");
+var filter_quantity = filter_quantity.replace(/&.*/, "");
+var filter_quantity = filter_quantity.replace(/=.*/, "");
 //Generate a list of results, and buttons to go to previous/next page
-document.body.onload = create_content_list(12);
-function create_content_list (items_per_page, keyword_filters, type_filters) {
+document.body.onload = create_content_list(filter_quantity, filter_page_number, filter_keywords, filter_types);
+function create_content_list (quantity, page_number, keywords, types) {
 	//Create list
 	const content_list = document.createElement("div");
 	//Add class
@@ -16,13 +26,23 @@ function create_content_list (items_per_page, keyword_filters, type_filters) {
 	const selected_element = document.getElementById("content");
 	document.body.insertBefore(content_list, selected_element);
 	//Read database text file to get list of posts, and pages
-
-	//Filter list 
-  
-  //Shrink list to required size, starting from required offset
-
+	var database = 'post1|/post1.html|post\npost2|/post2.html|post\npage1/page1.html|page';
+	var database = database.split('\n');
+	//Determine offset to start collecting results (multiply the page number by the quantity of results per page)
+	var offset = quantity * page_number;
+	//Filter list (starting from the offset, find the first $quantity results which contain the keywords, and match any of the selected types)
+	var window.database_results = [];
+	database.forEach(check_keywords_and_types);
+	function check_keywords_and_types(item, index) {
+		///Check type
+		
+		///Check keywords
+		
+		///If useable add to global database_results array
+		
+	}
 	//Generate a list entry for each item
-
+	
 	//Create buttons for navigating to previous (if applicable), and next offset
 
 }
